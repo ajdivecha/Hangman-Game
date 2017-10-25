f233de2627d7f115ac4541f068efa5ce174c8966
Adrian Divecha <ajdivecha@macbook-pro-2.home>
2017-10-24 23:14:10 -0700

    HMG-1

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//the array that holds the words we choose from
+
+var words = ["bulldog", "labrador", "poodle", "boxer", "terrier"];
+
+//word picker function
+
+function chooseWord (){
+
+}
+
+
+
+
+
